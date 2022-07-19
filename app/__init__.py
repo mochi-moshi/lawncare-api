41588f8d4ddab61ba12119d991802534e583bd2b
mochi-moshi <30363079+mochi-moshi@users.noreply.github.com>
2022-07-19 00:57:04 -0500

    Moved main.py into __init__.py and updated requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
-from .main import app
+from fastapi import FastAPI
+from .routers import client
+
+app = FastAPI()
+app.include_router(client.router)
+
+@app.get('/')
+def root():
+    return {}
+
+@app.get('/test')
+def test():
+    return {"message": "Hello, World!"}
